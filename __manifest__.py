a2c6fc617c2b1091ffd70ae75e63bf3e8ca9de1d
Kevin Kong <kfx2007@outlook.com>
2021-11-24 09:56:35 +0800

    15.1.3

--- __manifest__.py
+++ __manifest__.py
@@ -17,7 +17,7 @@
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'tutorial',
-    'version': '15.2',
+    'version': '15.1.3',
 
     # any module necessary for this one to work correctly
     'depends': ['base'],
